d94d458583ea9c1796f665cf3f1e5d65da3b9e63
anusharlove <asuanu2105@gmail.com>
2026-06-02 08:58:47 +0530

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,1 +1,26 @@
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from database.db import get_tickets
+
+
+async def main_menu(message, user_id):
+
+    tickets = await get_tickets(user_id)
+
+    buttons = [
+        [
+            InlineKeyboardButton("👤 MY INFO", callback_data="myinfo"),
+            InlineKeyboardButton("🏆 LEADERBOARD", callback_data="leaderboard")
+        ],
+        [
+            InlineKeyboardButton("🎁 REDEEM CODE", callback_data="redeem")
+        ],
+        [
+            InlineKeyboardButton("🎉 BONUS", callback_data="bonus")
+        ]
+    ]
+
+    await message.reply_text(
+        f"🎟️ SF GIVEAWAY PANEL\n\n🎫 YOUR TICKETS: {tickets}",
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
