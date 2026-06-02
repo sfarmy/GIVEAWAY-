27cf21412a6d295b270ab33c9314380724e19447
anusharlove <asuanu2105@gmail.com>
2026-06-02 08:58:08 +0530

    Update leaderboard.py

--- handlers/leaderboard.py
+++ handlers/leaderboard.py
@@ -1,1 +1,26 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from database.db import top_users
 
+
+async def open_leaderboard(query):
+
+    users = await top_users()
+
+    text = "🏆 TOP 15 USERS\n\n"
+
+    rank = 1
+
+    for user in users:
+
+        username = user[0] or "Unknown User"
+        tickets = user[1]
+
+        text += f"{rank}. {username} ➜ 🎟️ {tickets}\n"
+        rank += 1
+
+    await query.message.edit_text(
+        text,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("🔙 BACK", callback_data="back")]
+        ])
+    )
